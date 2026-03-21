854e125b6885bbbdb3f5c1e0bc994c54305443d9
AkhileshRawatGit <akhileshrawat242@gmail.com>
2026-03-21 23:05:55 +0530

    understand the java underscore concept

--- basic program/add.cpp
+++ basic program/add.cpp
@@ -1,10 +1,9 @@
 #include<iostream>
 using namespace std;
 int main(){
-    float a= 0.2;
-    float b= 0.3;
-    float c= a+b;
-    if(c==0.5) cout<<"tr";
-    else cout<<"f";
+    //int a=12_3456_789; //it give the error in c++ but not in java
+    int a=123456789;
+    cout<<a;
+
     return 0;
 }
