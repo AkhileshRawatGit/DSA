06faac37ec2ef5da34b72756121d236b0e16bc2c
Akhilesh Rawat <akhileshrawat242@gmail.com>
2026-07-27 18:37:03 +0530

    Time: 10 ms (33.88%) | Memory: 13.2 MB (60.47%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -14,10 +14,32 @@ public:
             return dp[n][m]=min(deleted,min(inserted,replaced));
         }
     }
+
+//bottom up 
+//dp[i][j]=dp[i-1][j-1]
+//dp[i][j]=min(1+dp[i-1][j],min(1+dp[i][j-1],1+dp[i-1][j-1]))
     int minDistance(string word1, string word2) {
         int n=word1.size();
         int m=word2.size();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        return minDist(n,m,word1,word2,dp);
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        //return minDist(n,m,word1,word2,dp);
+        //column zero
+        for(int i=0;i<=n;i++){
+            dp[i][0]=i;
+        }
+        for(int i=0;i<=m;i++){
+            dp[0][i]=i;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(word1[i-1]==word2[j-1]){
+                    dp[i][j]=dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=min(1+dp[i-1][j],min(1+dp[i][j-1],1+dp[i-1][j-1]));
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
