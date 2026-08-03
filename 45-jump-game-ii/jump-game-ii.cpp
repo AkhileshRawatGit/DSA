ab241993f78e3ee7e15da8d270781daa3ce6c043
Akhilesh Rawat <akhileshrawat242@gmail.com>
2026-08-03 13:07:09 +0530

    Time: 64 ms (18.40%) | Memory: 22.4 MB (10.22%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,18 +1,30 @@
 class Solution {
 public:
-    long long reachEnd(vector<int>&nums, long long ind,vector<int>&dp){
-        if(ind>=nums.size()-1) return 0;
-        if(dp[ind]!=-1) return dp[ind];
-        long long ans=INT_MAX;
-        for(int i=1;i<=nums[ind];i++){
-                long long count=1+reachEnd(nums,ind+i,dp);
-                ans=min(ans,count);
+    long long reachEnd(vector<int>& nums, long long ind, vector<int>& dp) {
+        if (ind >= nums.size() - 1)
+            return 0;
+        if (dp[ind] != -1)
+            return dp[ind];
+        long long ans = INT_MAX;
+        for (int i = 1; i <= nums[ind]; i++) {
+            long long count = 1 + reachEnd(nums, ind + i, dp);
+            ans = min(ans, count);
         }
-        return dp[ind]=ans;
+        return dp[ind] = ans;
     }
     int jump(vector<int>& nums) {
-        long long ind=0;
-        vector<int>dp(nums.size()+1,-1);
-        return reachEnd(nums,ind,dp);      
+        int n = nums.size();
+        vector<long long> dp(n, INT_MAX);
+        dp[n - 1] = 0;
+        long long ind = n-1;
+        while (ind>=0) {
+            for (int jump = 1; jump <= nums[ind];jump++) {
+                if(ind+jump<n)
+                    dp[ind] = min(dp[ind],1+dp[ind+jump]);
+            }
+            ind--;
+        }
+        return dp[0];
     }
-};
+}
+;
