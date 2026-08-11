03eebd685917c34ac50f6fd4762865552f6e584f
Akhilesh Rawat <akhileshrawat242@gmail.com>
2026-08-11 20:46:14 +0530

    Time: 192 ms (45.91%) | Memory: 12.7 MB (64.61%) - LeetSync

---
+++ 204-count-primes/count-primes.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        vector<bool>visited(n,0);
+        if(n<=1) return 0;
+        int count=0;
+        for(int i=2;i<n;i++){
+            if(!visited[i]){
+                count++;
+                for(long long j=1LL*i*i;j<n;j+=i){
+                    visited[j]=1;
+                }
+            }
+        }
+        return count;
+    }
+};
